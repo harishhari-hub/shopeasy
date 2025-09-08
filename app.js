c01ccf6476b4fd7daf532d7450fe7d9e4f2cb7d5
harishhari-hub <harishirah143@gmail.com>
2025-09-08 06:37:51 +0530

    add CI pipeline with Github actions

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.get("/",(req,res)=>{
     res.send("Welcome to shopeasy from harish website!");
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
+module.exports = {app,server}
